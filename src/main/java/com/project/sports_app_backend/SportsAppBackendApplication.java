fc25e728cedf161a68dd8d0cd4605a36ab2db784
DagmaraSkorupska <dagmaraskorupska@gmail.com>
2021-01-23 13:47:45 +0100

    first commit

---
+++ src/main/java/com/project/sports_app_backend/SportsAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.sports_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsAppBackendApplication.class, args);
+    }
+
+}
